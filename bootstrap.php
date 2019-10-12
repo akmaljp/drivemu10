2d502b2a3f0e49866e3b4a2db0a29ea44e869a8c
akmaljp <46837901+akmaljp@users.noreply.github.com>
2019-10-12 13:51:35 +0700

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+* @package   akmaljp\drivemu
+* @copyright Copyright (c) 2015-2018 The akmaljp Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace akmaljp\drivemu;
+
+use Flarum\Formatter\Event\Configuring;
+use Illuminate\Events\Dispatcher;
+use s9e\TextFormatter\Configurator\Bundles\MediaPack;
+
+function subscribe(Dispatcher $events)
+{
+	$events->listen(
+		Configuring::class,
+		function (Configuring $event)
+		{
+			$event->configurator->MediaEmbed->add(
+				'drivemu',
+				[
+					'host'    => 'drivemu.com',
+					'extract' => "!drivemu\\.com/download/(?'id'[-0-9A-Z_a-z]+)!",
+					'iframe'  => ['src' => 'https://drivemu.com/download/{@id}/']
+				]
+			);
+		}
+	);
+}
+
+return __NAMESPACE__ . '\\subscribe';
